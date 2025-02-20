4e77753f06366199e5e63fbe646467cba41f0c5d
aboubakrndong <aboubakr.ndong@gmail.com>
2025-02-20 10:25:28 +0100

    Refactor: Simplify AppComponent by removing server state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { ServerService } from './service/server.service';
-import { catchError, map, Observable, of, startWith } from 'rxjs';
-import { AppState } from './interface/app-state';
-import { CustomResponse } from './interface/custom-response';
-import { DataState } from './enum/data-state.enum';
-import { Status } from './enum/status.enum';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  appState$: Observable<AppState<CustomResponse>> | undefined;
-  readonly DataState = DataState;
-  readonly Status = Status;
-
-  constructor(private serverService: ServerService) {
-
-  }
-
-  ngOnInit(): void {
-    this.appState$ = this.serverService.server$
-      .pipe(
-        map(response => {
-          return { dataState: DataState.LOADED_STATE, appData: response }
-        }),
-        startWith({ dataState: DataState.LOADING_STATE }),
-        catchError((error: string) => {
-          return of({ dataState: DataState.ERROR_STATE, error })
-        })
-      );
+export class AppComponent {
+  
+  constructor() {
   }
 
 }
